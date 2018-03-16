a767d6498d3defbe7a544788d2ec1052bfb72b44
DESKTOP-FCTP4LM\Vadim <az3249@gmail.com>
2018-03-16 17:38:32 +0200

    create green test

--- tst/TestDeposit.java
+++ tst/TestDeposit.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+import java.awt.*;
+import static org.junit.Assert.*;
+
 public class TestDeposit {
+
+    @Test
+    public void testDeposit(){
+
+
+        BancAccount account= new BancAccount ();
+        int inputValue = 50;
+        int expectedValue = 0;
+        account.deposite(inputValue);
+        expectedValue = account.getBalance();
+        assertEquals(expectedValue,inputValue);
+    }
+
 }
